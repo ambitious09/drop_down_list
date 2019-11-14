7d4d28be894ee57c43615de26423850ea5612796
wyjq <liuhuihua@wyjq.com>
2019-11-14 15:51:52 +0800

    测试

--- android/src/main/kotlin/come/drop_down_list/DropDownListPlugin.kt
+++ android/src/main/kotlin/come/drop_down_list/DropDownListPlugin.kt
@@ -1,25 +1,21 @@
-package come.drop_down_list
-
-import io.flutter.plugin.common.MethodCall
-import io.flutter.plugin.common.MethodChannel
-import io.flutter.plugin.common.MethodChannel.MethodCallHandler
-import io.flutter.plugin.common.MethodChannel.Result
-import io.flutter.plugin.common.PluginRegistry.Registrar
-
-class DropDownListPlugin: MethodCallHandler {
-  companion object {
-    @JvmStatic
-    fun registerWith(registrar: Registrar) {
-      val channel = MethodChannel(registrar.messenger(), "drop_down_list")
-      channel.setMethodCallHandler(DropDownListPlugin())
-    }
-  }
-
-  override fun onMethodCall(call: MethodCall, result: Result) {
-    if (call.method == "getPlatformVersion") {
-      result.success("Android ${android.os.Build.VERSION.RELEASE}")
-    } else {
-      result.notImplemented()
-    }
-  }
-}
+//package come.drop_down_list
+//
+//
+//
+//class DropDownListPlugin: MethodCallHandler {
+//  companion object {
+//    @JvmStatic
+//    fun registerWith(registrar: Registrar) {
+//      val channel = MethodChannel(registrar.messenger(), "drop_down_list")
+//      channel.setMethodCallHandler(DropDownListPlugin())
+//    }
+//  }
+//
+//  override fun onMethodCall(call: MethodCall, result: Result) {
+//    if (call.method == "getPlatformVersion") {
+//      result.success("Android ${android.os.Build.VERSION.RELEASE}")
+//    } else {
+//      result.notImplemented()
+//    }
+//  }
+//}
