394c8498d7aa2755571bafa923eb1a75f437b29d
wyjq <liuhuihua@wyjq.com>
2019-11-20 10:43:14 +0800

    修复列表项的值为空

--- lib/tools.dart
+++ lib/tools.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 
 class Utils{
   static Color getBackgroundColor(BuildContext context){
+    print("1");
     return Theme.of(context).scaffoldBackgroundColor;
   }
 }
